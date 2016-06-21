0bec6a42d92226d508fe59271d64a61b603bc22d
whaozl <zhulin.hao@huaat.net>
2016-06-21 09:35:02 +0800

    44

---
+++ anjos/anjos-persist/src/main/java/com/whaozl/anjos/anjos_persist/App.java
@@ -0,0 +1,13 @@
+package com.whaozl.anjos.anjos_persist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ anjos/anjos-util/src/main/java/com/whaozl/anjos/anjos_util/App.java
@@ -0,0 +1,13 @@
+package com.whaozl.anjos.anjos_util;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
